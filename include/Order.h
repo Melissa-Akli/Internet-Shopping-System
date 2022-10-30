a463dcf3d89bf1f7cec0fd389a042119a38bc6ce
melissa <melissa.akli@ensia.edu.dz>
2022-10-30 11:18:13 +0100

    Second version with additional functions

--- include/Order.h
+++ include/Order.h
@@ -1,16 +1,23 @@
+#include<list>
+
+
 #ifndef ORDER_H
 #define ORDER_H
-
 
 class Order
 {
     public:
-        Order();
-        virtual ~Order();
+        Order();                       // NEED WORK
+        virtual ~Order();             // NEED WORK
+        void DeleteItem();                 // NEED WORK
+        void AddItem();                      // NEED WORK
+        void Display_PurshaseList();        // NEED WORK
+
 
 
 
     private:
+   // list <Item> PurshaseList;   // define item structure and make order friend of other classes
 
 };
 
--- include/Shop.h
+++ include/Shop.h
@@ -17,12 +17,18 @@ class Shop
         Shop();
         ~Shop();
         void AddRequest(); // by membership and time ...
-        string CheckRequest();
+        bool CheckRequest(); // before serving the customer
 
+
+
+    void remove_request(); // the online shop will serve the customer : check if it exists then check if memeber , increase total purchase
+    int delivery(); // will use the remove_request function  , calculate distance and return price
+    int bill();// price of delivery +purchase+ , increase total purchase
+    bool pay_bill();// check if the bill is successfully payed
 
 
     private:
-        queue <Order>Request;
+        //queue <Order>Request;
         list<Category> MyShop;
 };
 
