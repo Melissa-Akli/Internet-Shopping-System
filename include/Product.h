88bac7351b652ef3805f2e58504a00ae50c2760a
LynaSb <116938894+LynaSb@users.noreply.github.com>
2022-10-31 18:05:52 +0100

    Update Product.h

--- include/Product.h
+++ include/Product.h
@@ -1,22 +1,44 @@
-#include<iostream>
-using namespace std;
-
 #ifndef PRODUCT_H
 #define PRODUCT_H
 
+#include <string>
+using std::string;
 
 class Product
 {
     public:
-        Product();
-         ~Product();
+
+        Product();    //the default constructor
+        Product(string,string,double,long int);   //the parameterized constructor
+        ~Product();   //the destructor
+
+
+        void setAvailable_quantity(long int);   // to unable the shop to insert and update the available quantity
+        void setUnit_price(double);   //so that the shop can change the unit price of a product easily
+        void setProduct_ID(string);
+
+
+
+        long int getAvailable_quantity(void)const;
+        double getUnitprice(void)const;
+        string getProduct_ID(void)const;
+
+
+        void ProductDescription(void)const;      //to print the information about the product
+
 
 
     private:
          string name;
          string ID;
-         int unitprice;
-         int quantity;
+         double unitprice;
+         long int  available_quantity;  //I used a long int because it may be a colossal number
+         bool existentID(string)const
+         {
+           //no implementation for the moment
+
+
+         };     // I used here a utility function to check if the ID exists before or not
 
 };
 
